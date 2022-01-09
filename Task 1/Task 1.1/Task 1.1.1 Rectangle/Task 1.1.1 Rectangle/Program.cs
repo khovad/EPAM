c8d7ddefcb8a932fa6a225a576eef113cb56fdbf
Khovrin Vadim <khovrin.vadim.jr@gmail.com>
2022-01-09 19:44:50 +0300

    add task 1.1
    Add tasks 1.1.1-1.1.10

---
+++ Task 1/Task 1.1/Task 1.1.1 Rectangle/Task 1.1.1 Rectangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_1._1._1_Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            Console.Write("a = ");
+            int.TryParse(Console.ReadLine(), out a);
+            Console.Write("b = ");
+            int.TryParse(Console.ReadLine(), out b);
+            if ( a >0 && b > 0)
+            {
+                Console.WriteLine("Длина: {0}, Ширина: {1}, Площадь = {2}", a, b, a * b);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка. Неверные данные.");
+            }
+        }
+    }
+}
---
+++ Task 1/Task 1.1/Task 1.1.7 Array processing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_1._1._7_Array_processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Длина массива: ");
+            int N = int.Parse(Console.ReadLine());
+            int[] array = new int[N];
+            int max = 0;
+            int min = 0;
+            Random rand = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                array[i] = rand.Next(-50,50);
+                Console.WriteLine("Элемент массива " + i + " = " + array[i]);
+            }
+            min = array[0];
+            max = array[0];
+            for (int i = 1; i < N; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+                if (array[i] > max)
+                    max = array[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine("Максимальное значение: " + max);
+            Console.WriteLine("Минимальное значение: " + min);
+        }
+    }
+}
